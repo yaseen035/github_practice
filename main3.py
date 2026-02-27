b5403e632d0b9727e5d0bd3f2ff9e60e32ede89e
yaseen035 <yaseensidd048@gamil.com>
2026-02-27 22:01:06 +0500

    Deleted main2 and main3 .py files

--- main3.py
+++
@@ -1,9 +0,0 @@
-# write a random program for testing purposes   
-
-import random  
-def random_program():
-    print("Welcome to the random program!")
-    print("Here are some random numbers:")
-    for i in range(5):
-        print(random.randint(1, 100))
-    print("Thank you for using the random program!")
