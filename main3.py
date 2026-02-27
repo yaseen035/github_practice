e4adcb49a5ef8de74a88cc86ee792f9c594b796f
yaseen035 <yaseensidd048@gamil.com>
2026-02-27 21:58:04 +0500

    Added python3

---
+++ main3.py
@@ -0,0 +1,9 @@
+# write a random program for testing purposes   
+
+import random  
+def random_program():
+    print("Welcome to the random program!")
+    print("Here are some random numbers:")
+    for i in range(5):
+        print(random.randint(1, 100))
+    print("Thank you for using the random program!")
